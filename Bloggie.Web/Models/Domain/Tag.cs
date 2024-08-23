8e341d203cf621fe34f9e65c8bdfc60b5dfb6267
Tushar Mankar <tusharmankar02@gmail.com>
2024-08-23 17:50:26 +0530

    create database columes

---
+++ Bloggie.Web/Models/Domain/Tag.cs
@@ -0,0 +1,10 @@
+﻿namespace Bloggie.Web.Models.Domain
+{
+	public class Tag
+	{
+		public string Name { get; set; }
+		public string DisplayName { get; set; }
+
+		public ICollection<BlogPost> BlogPosts { get; set; }	
+	}
+}
