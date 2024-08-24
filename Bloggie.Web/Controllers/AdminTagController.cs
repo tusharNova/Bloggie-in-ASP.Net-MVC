a5fbe9df216daf54959c67f4a9d71012c17dea5a
Tushar Mankar <tusharmankar02@gmail.com>
2024-08-24 20:37:58 +0530

    Add admin add tag pages

---
+++ Bloggie.Web/Controllers/AdminTagController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bloggie.Web.Controllers
+{
+	public class AdminTagController : Controller
+	{
+		[HttpGet]
+		public IActionResult Add()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult SubmitTag() 
+		{
+			var name = Request.Form["name"];
+			var displayName = Request.Form["displayName"];
+			return View("Add");
+		}
+	}
+}
